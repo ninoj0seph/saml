7824caee009347dc6245dcf40f74cd14b3e33003
ninoj0seph <ninojoseph.tugade@gmail.com>
2017-09-14 11:40:34 -0700

    test

--- www/test.php
+++ www/test.php
@@ -4,13 +4,17 @@
   $as = new SimpleSAML_Auth_Simple('default-sp');
   $as->requireAuth();
   $attributes = $as->getAttributes();
+    if (!$as->isAuthenticated()) {
+        /* Show login link. */
+        print('<a href="/login">Login</a>');
+    }
 ?>
-<div style="font-weight: bold;">Hello, PHP World</div>
-<table border="1">
-    <?php  foreach ($attributes as $key => $value): ?>
-        <tr>
-            <td><?=$key;?></td>
-            <td><?=$value[0];?></td>
-        </tr>
-    <?php endforeach;?>
-</table>
+<!--<div style="font-weight: bold;">Hello, PHP World</div>-->
+<!--<table border="1">-->
+<!--    --><?php // foreach ($attributes as $key => $value): ?>
+<!--        <tr>-->
+<!--            <td>--><?//=$key;?><!--</td>-->
+<!--            <td>--><?//=$value[0];?><!--</td>-->
+<!--        </tr>-->
+<!--    --><?php //endforeach;?>
+<!--</table>-->
